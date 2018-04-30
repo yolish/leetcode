23f4935b484b48e61d54a3c4365367136dc04ac0
yolish <yolisha@gmail.com>
2018-04-30 14:49:02 +0100

    added test cases

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import word_ladder_ii
 def test_solution(solution, test_set, description):
     print(description)
     for i, test_case in enumerate(test_set):
-        print("Test case {}:".format(i))
+        print("Test case {}:".format(i+1))
         print(test_case)
         print("Solution:")
         print(solution(*test_case))
@@ -13,9 +13,18 @@ def test_solution(solution, test_set, description):
 test_set = [
     ["hit","cog",["hot","dot","dog","lot","log","cog"]],
     ["hit", "cog", ["hot","dot","dog","lot","log"]],
-["hot", "dog", ["hot","cog","dog","tot","hog","hop","pot","dot"]]
+["hot", "dog", ["hot","cog","dog","tot","hog","hop","pot","dot"]],
+    ["hot","dog",["hot","dog","cog","pot","dot"]],
+    ["red", "tax", ["ted","tex","red","tax","tad","den","rex","pee"]]
 ]
-test_set = [test_set[2]]
+'''
+Output:
+[["red","ted","tex","tax"],["red","ted","tad","tax"]]
+Expected:
+[["red","ted","tad","tax"],["red","ted","tex","tax"],["red","rex","tex","tax"]]
+'''
+
+test_set = [test_set[-1]]
 solution = word_ladder_ii.Solution()
 test_solution(solution.findLadders, test_set, solution.description())
 
