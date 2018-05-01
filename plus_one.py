3b2b00319e47404bffa5dafa6e6afce0cb06e4a1
yolish <yolisha@gmail.com>
2018-05-01 13:31:47 +0100

    added description

--- plus_one.py
+++ plus_one.py
@@ -1,5 +1,10 @@
 #https://leetcode.com/problems/plus-one/description/
 class Solution(object):
+    def description(self):
+        desc = "Solution to plus-one problem: iterate from right to left: if the digit " \
+               "is smaller than 9 add 1 and break, otherwise set to zero and carry over. " \
+               "If you reached the end with a carry over add '1' on the left side (begining of the array)."
+        return desc
     def plusOne(self, digits):
         """
         :type digits: List[int]
