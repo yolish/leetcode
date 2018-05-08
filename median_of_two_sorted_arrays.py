8f5d32b0b0bf5f072814743307ce595ac1d522a0
yolish <yolisha@gmail.com>
2018-05-08 22:04:57 +0100

    cont. solution log(m+n)

--- median_of_two_sorted_arrays.py
+++ median_of_two_sorted_arrays.py
@@ -6,45 +6,30 @@ class Solution(object):
         desc = "Solution for the median-of-two-sorted-arrays problem"
         return desc
 
-    def get_median(self, l, is_single_median = None, is_left_median = True):
+    def get_median(self, l):
         median = 0.0
         indices = None
         n = len(l)
         if n > 0:
-            if is_single_median is None:
-                is_single_median = n%2 == 1
             middle = n/2
-            if is_single_median:
+            if n%2 == 1:
                 indices = [middle]
             else:
-                if is_left_median or n%2==0:
-                    indices = [middle-1, middle]
-                else:
-                    indices = [middle, middle+1]
+                indices = [middle-1, middle]
             median = sum([l[index] for index in indices]) / (len(indices) * 1.0)
         return median, indices
-
-
 
     def split(self, l, indices, take_first_half):
         k = len(l)
         if k > 2:
             if take_first_half:
                 start_index = 0
-                if k == 2:
-                    end_index = (max(indices))
-                else:
-                    end_index = (max(indices)+1)
+                end_index = (max(indices)+1)
             else:
+                start_index = min(indices)
                 end_index = len(l)
-                if k == 2:
-                    start_index = max(indices)
-                else:
-                    start_index = min(indices)
             l = l[start_index:end_index]
-
         return l
-
 
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -54,28 +39,49 @@ class Solution(object):
         """
         m = len(nums1)
         n = len(nums2)
-        is_single_median = (m+n)%2 == 1
-        med1, indices1 = self.get_median(nums1)
-        med2, indices2 = self.get_median(nums2)
-        is_left_median = True
-        if n != m and (n+m)%2 == 0:
-            if n < m:
-                is_left_median = med1 <= med2
-            else:
-                is_left_median = med2 <= med1
-        while m+n > 4:
+        if m==0:
+            median, indices = self.get_median(nums2)
+        elif n==0:
+            median, indices = self.get_median(nums1)
+        else:
+            is_single_median = (m+n)%2 == 1
             med1, indices1 = self.get_median(nums1)
-            med2, indices2 =  self.get_median(nums2)
-            nums1 = self.split(nums1, indices1, med1 >= med2)
-            nums2 = self.split(nums2, indices2, med2 > med1)
-            m = len(nums1)
-            n = len(nums2)
-        # 0 <= n + m <= 4
-        print nums1
-        print nums2
-        nums = sorted(nums1 + nums2)
-        median, indices = self.get_median(nums, is_single_median, is_left_median)
-        return median
+            med2, indices2 = self.get_median(nums2)
+            med_of_med = (med1*m+med2*n)/(m+n)
+            while m > 2 or n > 2:
+                med1, indices1 = self.get_median(nums1)
+                med2, indices2 = self.get_median(nums2)
+                nums1 = self.split(nums1, indices1, med1 > med_of_med)
+                nums2 = self.split(nums2, indices2, med2 > med_of_med)
+                m = len(nums1)
+                n = len(nums2)
+
+            nums = sorted(nums1+nums2)
+            median, indices = self.get_median(nums)
+            if is_single_median and len(nums)%2 == 0:
+                diff1 = abs(nums[indices[0]]-med_of_med)
+                diff2 = abs(nums[indices[1]]-med_of_med)
+                if diff1 < diff2:
+                    median = nums[indices[0]]
+                else:
+                    median = nums[indices[1]]
+            elif not is_single_median and len(nums)%2 == 1:
+                diff = [abs(elem-med_of_med) for elem in nums]
+                sorted_diff = sorted(diff)
+                indices = []
+                found_indices = False
+                for sval in sorted_diff:
+                    for i, val in enumerate(diff):
+                        if val == sval:
+                            indices.append(i)
+                        if len(indices) == 2:
+                            found_indices = True
+                            break
+                    if found_indices:
+                        break
+                median_elems = [nums[index] for index in indices]
+                median = sum(median_elems)/2.0
+        return median*1.0
 
 
 
